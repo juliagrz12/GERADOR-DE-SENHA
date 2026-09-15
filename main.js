2a2818f82d737155ce1f18ae5515f63a7a5616cd
juliagrz12 <julia.grzybowski.silva@escola.pr.gov.br>
2026-09-15 11:48:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,49 @@ const focaSenha = document.querySelector('forca');
 
 botoes[0].onlick = diminuiTamanho;
 botoes[1].onlick = aumentaTamanho;
+function diminuiTamanho(){
+    if (tamanhoSenha> 1){
+        //tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function diminuiTamanho(){
+    if(tamanhoSenha,20){
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+for(i=0;<checkbox.lenght;i++){
+    checkbox[i].onlick = geraSenha;
+}
+geraSenha();
+function geraSenha(){
+}
+geraSenha();
+function geraSenha(){
+    let alfabeto = ''}
+    if(checkbox[0].checked){
+        alfabeto= alfabeto + letrasMaiusculas;
+
+    }
+    if(checkbox[1].checked){
+
+    }
+    if(checkbox[2].checked){
+
+    }
+    if(checkbox[3].checked){
+
+    }
+    let senha= '';
+    for(let i=0; i,tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*alfabeto.lenght;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio]
+    }    
+campoSenha.value = senha;
+classificaSenha
